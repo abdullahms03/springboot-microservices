e2488d0bdd374adc32e654448abde2538c66e389
Abdullah <abdullahms03@outlook.com>
2020-09-23 08:23:54 +1000

    Circuit breaker implementation - added Hystrix library

--- book-catalog-service/src/main/java/io/keeplearning/bookcatalogservice/resources/BookCatalogResource.java
+++ book-catalog-service/src/main/java/io/keeplearning/bookcatalogservice/resources/BookCatalogResource.java
@@ -1,5 +1,6 @@
 package io.keeplearning.bookcatalogservice.resources;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.keeplearning.bookcatalogservice.model.Book;
 import io.keeplearning.bookcatalogservice.model.CatalogItem;
 import io.keeplearning.bookcatalogservice.model.UserRating;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,24 +26,26 @@ public class BookCatalogResource {
     @Autowired
     WebClient.Builder webClientBuilder;
 
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
         UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratings/users/"+userId, UserRating.class);
+        return userRating.getUserRatings().stream()
+                .map(rating -> {
+                    Book book = restTemplate.getForObject("http://book-info-service/books/" + rating.getBookId(), Book.class);
+                    return new CatalogItem(book.getName(), book.getDescription(), rating.getRating());
+                })
+                .collect(Collectors.toList());
+    }
 
-        return userRating.getUserRatings().stream().map(rating -> {
-            Book book = restTemplate.getForObject("http://book-info-service/books/"+rating.getBookId(), Book.class);
+    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
+        return Arrays.asList(new CatalogItem("Book", "", 0));
+    }
+}
 
-            /*Book book = webClientBuilder.build()
+/*Book book = webClientBuilder.build()
                     .get()
                     .uri("http://book-info-service/books/"+rating.getBookId())
                     .retrieve()
                     .bodyToMono(Book.class)
                     .block();*/
-
-            return new CatalogItem(book.getName(), book.getDescription(), rating.getRating());
-        })
-                .collect(Collectors.toList());
-    }
-
-}
